efba0a6ad34b1d71a0395857c1d7eabac7d75115
david.walshe93@gmail.com <david.walshe93@gmail.com>
2020-04-08 22:55:23 +0100

    Added time graphing for performance review.

---
+++ src/data_loader.py
@@ -0,0 +1,129 @@
+"""
+Author:         David Walshe
+Date:           03/04/2020   
+"""
+
+# Imports
+from sklearn.datasets import fetch_kddcup99
+
+import numpy as np
+import pandas as pd
+from json import load
+
+
+class LabelManager:
+
+    def __init__(self, config_file="data.json"):
+        self._data_info = self._read_data_info(config_file=config_file)
+        self._X_column_names = None
+        self._y_column_name = None
+
+    @staticmethod
+    def _read_data_info(config_file) -> dict:
+        with open(config_file) as fh:
+            data_info = load(fp=fh)
+
+        return data_info
+
+    @property
+    def info(self):
+        return self._data_info
+
+    def _get_column_names(self, key: str) -> list:
+        return [item.get("name") for item in self.info.get(key)]
+
+    @property
+    def X_column_names(self):
+
+        # Lazy Init
+        if self._X_column_names is None:
+            self._X_column_names = self._get_column_names(key="X")
+
+        return self._X_column_names
+
+    @property
+    def y_column_name(self):
+
+        # Lazy Init
+        if self._y_column_name is None:
+            self._y_column_name = self._get_column_names(key="y")
+
+        return self._y_column_name
+
+    @property
+    def X_y_column_names(self) -> tuple:
+        """
+        Helper function for acquiring the dataset column names from data.json.
+
+        :return: The X columns names and the y column name as a tuple.
+        """
+        return self.X_column_names, self.y_column_name
+
+    def get_variable_on_dtype(self, key: str, dtype: str) -> list:
+        return [item.get("name") for item in self.info.get(key) if item.get("dtype") == dtype]
+
+    @property
+    def X_discreet(self):
+        return self.get_variable_on_dtype(key="X", dtype="discrete")
+
+    @property
+    def X_continuous(self):
+        return self.get_variable_on_dtype(key="X", dtype="continuous")
+
+
+class DataRetriever:
+
+    def __init__(self, label_manager: LabelManager):
+        self.label_manager = label_manager
+        self._X = None
+        self._y = None
+
+    def _remove_duplicate_rows(self):
+
+        dataset = pd.concat([self.X, self.y], axis=1, join="outer")
+
+        dataset1 = dataset.drop_duplicates(inplace=False)
+
+        self._X = pd.DataFrame(data=dataset1.iloc[:, :-1].values, columns=self.label_manager.X_column_names)
+
+        self._y = pd.DataFrame(data=dataset1.iloc[:, -1].values.reshape(-1, 1), columns=self.label_manager.y_column_name)
+
+
+    def X_y_dataset(self, remove_duplicates=False, full_dataset=True) -> np.array:
+        """
+        Helper function to create the dataset, including the dependant "target" variable.
+
+        :return: The dataset as an np.array, including the dependant variable.
+        """
+        # Lazy init
+        if self._X is None or self._y is None:
+            print(f"Only 10% of Dataset: {(not full_dataset)}")
+            data, target = fetch_kddcup99(return_X_y=True, percent10=(not full_dataset))
+
+            target = np.array(target).reshape(-1, 1)
+
+            self._X = pd.DataFrame(data=data, columns=self.label_manager.X_column_names)
+            self._y = pd.DataFrame(data=target, columns=self.label_manager.y_column_name)
+
+            if remove_duplicates:
+                self._remove_duplicate_rows()
+
+        return self._X, self._y
+
+    @property
+    def X(self):
+
+        # Lazy init
+        if self._X is None:
+            self.X_y_dataset()
+
+        return self._X
+
+    @property
+    def y(self):
+
+        # Lazy init
+        if self._y is None:
+            self.X_y_dataset()
+
+        return self._y
---
+++ src/timer.py
@@ -0,0 +1,46 @@
+"""
+Author:         David Walshe
+Date:           05/04/2020   
+"""
+
+from datetime import datetime as dt
+from collections import OrderedDict
+
+class Timer:
+
+    def __init__(self):
+        print("Timer Started")
+        self.initial_start_time = dt.now()
+        self.section_start_time = dt.now()
+        self.delta = None
+        self.time_log = OrderedDict()
+
+    def time_stage(self, stage):
+        end_time = dt.now()
+        self.delta = end_time - self.section_start_time
+        self._print_time(stage=stage)
+
+        self.time_log.update({stage: self.delta.total_seconds()})
+
+        self._reset()
+
+    def time_script(self):
+        end_time = dt.now()
+        self.delta = end_time - self.initial_start_time
+        self._print_time(stage="END")
+
+        self._reset()
+
+    def _reset(self):
+        self.section_start_time = dt.now()
+
+    def _print_time(self, stage):
+        minutes = round(self.delta.total_seconds() / 60)
+        seconds = round((self.delta.total_seconds() % 60))
+        microseconds = round(((self.delta.total_seconds() % 60) % 1) * 1000)
+
+        print(f"{minutes:02d}:{seconds:02d}.{microseconds:03d} - Stage: {stage}")
+
+    @property
+    def plot_data(self):
+        return list(self.time_log.keys()), list(self.time_log.values())
